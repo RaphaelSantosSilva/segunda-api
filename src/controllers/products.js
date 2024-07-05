321f00b13ef52eee2336fc8ccb229fa5b9363424
Raphael Santos <raaphaaelsaantos@gmail.com>
2024-07-04 22:04:35 -0300

    adicionando metodo POST

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -11,6 +11,27 @@ async function get(req,res) {
     res.send(products)
 }
 
+async function post(req,res){
+    const {
+        name,
+        brand,
+        price,
+    } = req.body
+
+    const products = new ProductsModel({
+        name,
+        brand,
+        price
+    })
+
+    products.save()
+
+    res.send({
+        message: 'Dados salvos com sucesso'
+    })
+}
+
 module.exports = {
     get,
+    post,
 }
